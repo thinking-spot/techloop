f0b226466848bf2c0be8662912de0c13a2c701d2
TechLoop Builder <builder@techloop.dev>
2025-12-31 11:47:29 -0600

    all set

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
           <CartDrawer />
 
           {/* Main Content Area - Offset for Sidebar and TopNav */}
-          <div className="flex flex-col min-h-screen md:pl-64 pt-16">
+          <div className="flex flex-col min-h-screen md:pl-64">
             <main className="flex-1 w-full max-w-7xl mx-auto p-4 md:p-8">
               {children}
             </main>
